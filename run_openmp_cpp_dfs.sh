3b147f53ce03566413e44f7936a60eb9463a325a
mac-yuhung94 <yuhung941121@gmail.com>
2025-12-19 18:40:15 +0800

    fix: correct solver path for 11x11 puzzle execution

--- run_openmp_cpp_dfs.sh
+++ run_openmp_cpp_dfs.sh
@@ -10,7 +10,7 @@ echo "Running with ${OMP_NUM_THREADS} threads"
 # python3 grader.py ./solver/openmp_solver_cpp ./data/7x7 120.0
 
 echo "Running on 11x11 puzzles"
-python3 grader.py ./solver/seq_solver_dfs_cpp ./data/11x11 10.0
+python3 grader.py ./solver/openmp_solver_dfs_cpp ./data/11x11 10.0
 
 # echo "Running on 20x20 puzzles"
 # python3 grader.py ./solver/openmp_solver_cpp ./data/20x20 300.0
